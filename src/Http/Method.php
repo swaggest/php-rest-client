bc2b03ee66b0a6bc95514ab4d21664e0563fb3d2
Viacheslav Poturaev <vearutop@gmail.com>
2019-09-10 23:27:22 +0200

    Add method

---
+++ src/Http/Method.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Swaggest\RestClient\Http;
+
+class Method
+{
+    const GET = 'get';
+    const PUT = 'put';
+    const POST = 'post';
+    const DELETE = 'delete';
+    const OPTIONS = 'options';
+    const HEAD = 'head';
+    const PATCH = 'patch';
+    const TRACE = 'trace';
+}
